a56bf50abb062a85030b65796e086e4247d443d1
Rachel Pratt <rachlpratt@gmail.com>
2023-10-07 12:53:44 -0400

    Add conjugate functions and tests for preterite and imperfect tenses

--- conjugate.py
+++ conjugate.py
@@ -27,3 +27,68 @@ def conjugate_present(verb, pronoun):
                 return stem + "an"
             else:
                 return stem + "en"
+
+
+def conjugate_preterite(verb, pronoun):
+    stem = verb.stem
+    match pronoun:
+        case "yo":
+            if verb.ending == "ar":
+                return stem + "é"
+            else:
+                return stem + "í"
+        case "tú":
+            if verb.ending == "ar":
+                return stem + "aste"
+            else:
+                return stem + "iste"
+        case "él/ella/Ud.":
+            if verb.ending == "ar":
+                return stem + "ó"
+            else:
+                return stem + "ió"
+        case "nosotros":
+            if verb.ending == "ar":
+                return stem + "amos"
+            else:
+                return stem + "imos"
+        case "vosotros":
+            if verb.ending == "ar":
+                return stem + "asteis"
+            else:
+                return stem + "isteis"
+        case "ellos/ellas/Uds.":
+            if verb.ending == "ar":
+                return stem + "aron"
+            else:
+                return stem + "ieron"
+
+
+def conjugate_imperfect(verb, pronoun):
+    stem = verb.stem
+    match pronoun:
+        case "yo" | "él/ella/Ud.":
+            if verb.ending == "ar":
+                return stem + "aba"
+            else:
+                return stem + "ía"
+        case "tú":
+            if verb.ending == "ar":
+                return stem + "abas"
+            else:
+                return stem + "ías"
+        case "nosotros":
+            if verb.ending == "ar":
+                return stem + "ábamos"
+            else:
+                return stem + "íamos"
+        case "vosotros":
+            if verb.ending == "ar":
+                return stem + "abais"
+            else:
+                return stem + "íais"
+        case "ellos/ellas/Uds.":
+            if verb.ending == "ar":
+                return stem + "aban"
+            else:
+                return stem + "ían"
--- test_verb.py
+++ test_verb.py
@@ -191,6 +191,310 @@ class TestVerb(unittest.TestCase):
         verb = Verb("ir")
         self.assertEqual(verb.conjugate("present", "nosotros"), "vamos")
 
+    def test_conjugate_preterite1(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ar verb in the preterite tense for the pronoun yo."""
+        verb = Verb("hablar")
+        self.assertEqual(verb.conjugate("preterite", "yo"), "hablé")
+
+    def test_conjugate_preterite2(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ar verb in the preterite tense for the pronoun tú."""
+        verb = Verb("hablar")
+        self.assertEqual(verb.conjugate("preterite", "tú"), "hablaste")
+
+    def test_conjugate_preterite3(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ar verb in the preterite tense for the pronoun él/ella/Ud."""
+        verb = Verb("hablar")
+        self.assertEqual(verb.conjugate("preterite", "él/ella/Ud."), "habló")
+
+    def test_conjugate_preterite4(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ar verb in the preterite tense for the pronoun nosotros."""
+        verb = Verb("hablar")
+        self.assertEqual(verb.conjugate("preterite", "nosotros"), "hablamos")
+
+    def test_conjugate_preterite5(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ar verb in the preterite tense for the pronoun vosotros."""
+        verb = Verb("hablar")
+        self.assertEqual(verb.conjugate("preterite", "vosotros"), "hablasteis")
+
+    def test_conjugate_preterite6(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ar verb in the preterite tense for the pronoun
+        ellos/ellas/Uds."""
+        verb = Verb("hablar")
+        self.assertEqual(verb.conjugate("preterite", "ellos/ellas/Uds."),
+                         "hablaron")
+
+    def test_conjugate_preterite7(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -er verb in the preterite tense for the pronoun yo."""
+        verb = Verb("beber")
+        self.assertEqual(verb.conjugate("preterite", "yo"), "bebí")
+
+    def test_conjugate_preterite8(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -er verb in the preterite tense for the pronoun tú."""
+        verb = Verb("beber")
+        self.assertEqual(verb.conjugate("preterite", "tú"), "bebiste")
+
+    def test_conjugate_preterite9(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -er verb in the preterite tense for the pronoun él/ella/Ud."""
+        verb = Verb("beber")
+        self.assertEqual(verb.conjugate("preterite", "él/ella/Ud."), "bebió")
+
+    def test_conjugate_preterite10(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -er verb in the preterite tense for the pronoun nosotros."""
+        verb = Verb("beber")
+        self.assertEqual(verb.conjugate("preterite", "nosotros"), "bebimos")
+
+    def test_conjugate_preterite11(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -er verb in the preterite tense for the pronoun vosotros."""
+        verb = Verb("beber")
+        self.assertEqual(verb.conjugate("preterite", "vosotros"), "bebisteis")
+
+    def test_conjugate_preterite12(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -er verb in the preterite tense for the pronoun
+        ellos/ellas/Uds."""
+        verb = Verb("beber")
+        self.assertEqual(verb.conjugate("preterite", "ellos/ellas/Uds."),
+                         "bebieron")
+
+    def test_conjugate_preterite13(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ir verb in the preterite tense for the pronoun yo."""
+        verb = Verb("vivir")
+        self.assertEqual(verb.conjugate("preterite", "yo"), "viví")
+
+    def test_conjugate_preterite14(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ir verb in the preterite tense for the pronoun tú."""
+        verb = Verb("vivir")
+        self.assertEqual(verb.conjugate("preterite", "tú"), "viviste")
+
+    def test_conjugate_preterite15(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ir verb in the preterite tense for the pronoun él/ella/Ud."""
+        verb = Verb("vivir")
+        self.assertEqual(verb.conjugate("preterite", "él/ella/Ud."), "vivió")
+
+    def test_conjugate_preterite16(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ir verb in the preterite tense for the pronoun nosotros."""
+        verb = Verb("vivir")
+        self.assertEqual(verb.conjugate("preterite", "nosotros"), "vivimos")
+
+    def test_conjugate_preterite17(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ir verb in the preterite tense for the pronoun vosotros."""
+        verb = Verb("vivir")
+        self.assertEqual(verb.conjugate("preterite", "vosotros"), "vivisteis")
+
+    def test_conjugate_preterite18(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ir verb in the preterite tense for the pronoun
+        ellos/ellas/Uds."""
+        verb = Verb("vivir")
+        self.assertEqual(verb.conjugate("preterite", "ellos/ellas/Uds."),
+                         "vivieron")
+
+    def test_conjugate_preterite19(self):
+        """Verifies that the conjugate method correctly conjugates an
+        irregular verb in the preterite tense for the pronoun yo."""
+        verb = Verb("ir")
+        self.assertEqual(verb.conjugate("preterite", "yo"), "fui")
+
+    def test_conjugate_preterite20(self):
+        """Verifies that the conjugate method correctly conjugates an
+        irregular verb in the preterite tense for the pronoun tú."""
+        verb = Verb("ser")
+        self.assertEqual(verb.conjugate("preterite", "tú"), "fuiste")
+
+    def test_conjugate_preterite21(self):
+        """Verifies that the conjugate method correctly conjugates an
+        irregular verb in the preterite tense for the pronoun él/ella/Ud."""
+        verb = Verb("decir")
+        self.assertEqual(verb.conjugate("preterite", "él/ella/Ud."), "dijo")
+
+    def test_conjugate_preterite22(self):
+        """Verifies that the conjugate method correctly conjugates an
+        irregular verb in the preterite tense for the pronoun nosotros."""
+        verb = Verb("ir")
+        self.assertEqual(verb.conjugate("preterite", "nosotros"), "fuimos")
+
+    def test_conjugate_preterite23(self):
+        """Verifies that the conjugate method correctly conjugates an
+        irregular verb in the preterite tense for the pronoun vosotros."""
+        verb = Verb("hacer")
+        self.assertEqual(verb.conjugate("preterite", "vosotros"), "hicisteis")
+
+    def test_conjugate_preterite24(self):
+        """Verifies that the conjugate method correctly conjugates an
+        irregular verb in the preterite tense for the pronoun
+        ellos/ellas/Uds."""
+        verb = Verb("ser")
+        self.assertEqual(verb.conjugate("preterite", "ellos/ellas/Uds."),
+                         "fueron")
+
+    def test_conjugate_imperfect1(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ar verb in the imperfect tense for the pronoun yo."""
+        verb = Verb("hablar")
+        self.assertEqual(verb.conjugate("imperfect", "yo"), "hablaba")
+
+    def test_conjugate_imperfect2(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ar verb in the imperfect tense for the pronoun tú."""
+        verb = Verb("hablar")
+        self.assertEqual(verb.conjugate("imperfect", "tú"), "hablabas")
+
+    def test_conjugate_imperfect3(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ar verb in the imperfect tense for the pronoun él/ella/Ud."""
+        verb = Verb("hablar")
+        self.assertEqual(verb.conjugate("imperfect", "él/ella/Ud."), "hablaba")
+
+    def test_conjugate_imperfect4(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ar verb in the imperfect tense for the pronoun nosotros."""
+        verb = Verb("hablar")
+        self.assertEqual(verb.conjugate("imperfect", "nosotros"), "hablábamos")
+
+    def test_conjugate_imperfect5(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ar verb in the imperfect tense for the pronoun vosotros."""
+        verb = Verb("hablar")
+        self.assertEqual(verb.conjugate("imperfect", "vosotros"), "hablabais")
+
+    def test_conjugate_imperfect6(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ar verb in the imperfect tense for the pronoun
+        ellos/ellas/Uds."""
+        verb = Verb("hablar")
+        self.assertEqual(verb.conjugate("imperfect", "ellos/ellas/Uds."),
+                         "hablaban")
+
+    def test_conjugate_imperfect7(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -er verb in the imperfect tense for the pronoun yo."""
+        verb = Verb("beber")
+        self.assertEqual(verb.conjugate("imperfect", "yo"), "bebía")
+
+    def test_conjugate_imperfect8(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -er verb in the imperfect tense for the pronoun tú."""
+        verb = Verb("beber")
+        self.assertEqual(verb.conjugate("imperfect", "tú"), "bebías")
+
+    def test_conjugate_imperfect9(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -er verb in the imperfect tense for the pronoun él/ella/Ud."""
+        verb = Verb("beber")
+        self.assertEqual(verb.conjugate("imperfect", "él/ella/Ud."), "bebía")
+
+    def test_conjugate_imperfect10(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -er verb in the imperfect tense for the pronoun nosotros."""
+        verb = Verb("beber")
+        self.assertEqual(verb.conjugate("imperfect", "nosotros"), "bebíamos")
+
+    def test_conjugate_imperfect11(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -er verb in the imperfect tense for the pronoun vosotros."""
+        verb = Verb("beber")
+        self.assertEqual(verb.conjugate("imperfect", "vosotros"), "bebíais")
+
+    def test_conjugate_imperfect12(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -er verb in the imperfect tense for the pronoun
+        ellos/ellas/Uds."""
+        verb = Verb("beber")
+        self.assertEqual(verb.conjugate("imperfect", "ellos/ellas/Uds."),
+                         "bebían")
+
+    def test_conjugate_imperfect13(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ir verb in the imperfect tense for the pronoun yo."""
+        verb = Verb("vivir")
+        self.assertEqual(verb.conjugate("imperfect", "yo"), "vivía")
+
+    def test_conjugate_imperfect14(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ir verb in the imperfect tense for the pronoun tú."""
+        verb = Verb("vivir")
+        self.assertEqual(verb.conjugate("imperfect", "tú"), "vivías")
+
+    def test_conjugate_imperfect15(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ir verb in the imperfect tense for the pronoun él/ella/Ud."""
+        verb = Verb("vivir")
+        self.assertEqual(verb.conjugate("imperfect", "él/ella/Ud."), "vivía")
+
+    def test_conjugate_imperfect16(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ir verb in the imperfect tense for the pronoun nosotros."""
+        verb = Verb("vivir")
+        self.assertEqual(verb.conjugate("imperfect", "nosotros"), "vivíamos")
+
+    def test_conjugate_imperfect17(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ir verb in the imperfect tense for the pronoun vosotros."""
+        verb = Verb("vivir")
+        self.assertEqual(verb.conjugate("imperfect", "vosotros"), "vivíais")
+
+    def test_conjugate_imperfect18(self):
+        """Verifies that the conjugate method correctly conjugates a
+        regular -ir verb in the imperfect tense for the pronoun
+        ellos/ellas/Uds."""
+        verb = Verb("vivir")
+        self.assertEqual(verb.conjugate("imperfect", "ellos/ellas/Uds."),
+                         "vivían")
+
+    def test_conjugate_imperfect19(self):
+        """Verifies that the conjugate method correctly conjugates an
+        irregular verb in the imperfect tense for the pronoun yo."""
+        verb = Verb("ir")
+        self.assertEqual(verb.conjugate("imperfect", "yo"), "iba")
+
+    def test_conjugate_imperfect20(self):
+        """Verifies that the conjugate method correctly conjugates an
+        irregular verb in the imperfect tense for the pronoun tú."""
+        verb = Verb("ser")
+        self.assertEqual(verb.conjugate("imperfect", "tú"), "eras")
+
+    def test_conjugate_imperfect21(self):
+        """Verifies that the conjugate method correctly conjugates an
+        irregular verb in the imperfect tense for the pronoun él/ella/Ud."""
+        verb = Verb("ser")
+        self.assertEqual(verb.conjugate("imperfect", "él/ella/Ud."), "era")
+
+    def test_conjugate_imperfect22(self):
+        """Verifies that the conjugate method correctly conjugates an
+        irregular verb in the imperfect tense for the pronoun nosotros."""
+        verb = Verb("ir")
+        self.assertEqual(verb.conjugate("imperfect", "nosotros"), "íbamos")
+
+    def test_conjugate_imperfect23(self):
+        """Verifies that the conjugate method correctly conjugates an
+        irregular verb in the imperfect tense for the pronoun vosotros."""
+        verb = Verb("ser")
+        self.assertEqual(verb.conjugate("imperfect", "vosotros"), "erais")
+
+    def test_conjugate_imperfect24(self):
+        """Verifies that the conjugate method correctly conjugates an
+        irregular verb in the imperfect tense for the pronoun
+        ellos/ellas/Uds."""
+        verb = Verb("ir")
+        self.assertEqual(verb.conjugate("imperfect", "ellos/ellas/Uds."),
+                         "iban")
+
 
 if __name__ == "__main__":
     unittest.main()
