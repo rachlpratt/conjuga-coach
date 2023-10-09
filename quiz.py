ae512104d1f7bac52984f41f033efceada610817
Rachel Pratt <rachlpratt@gmail.com>
2023-10-09 19:17:18 -0400

    Update QuizItem class to handle instance attribute validation

--- quiz.py
+++ quiz.py
@@ -1,24 +1,56 @@
+from verb import Verb
+from utils import is_valid_tense, is_valid_pronoun
+
+
 class QuizItem:
-    def __init__(self, question, answer):
-        # The question will be a tuple containing a Verb object, tense, and pronoun
-        # The answer will be the conjugated form
-        self._question_verb = question[0]
-        self._question_tense = question[1]
-        self._question_pronoun = question[2]
-        self._answer = answer
+    def __init__(self, question: tuple[Verb, str, str], answer: str) -> None:
+        """Initialize a QuizItem with a question and an answer."""
+        self._question_verb = None
+        self._question_tense = None
+        self._question_pronoun = None
+        self.update_question(question)
+        self.answer = answer
+
+    def update_question(self, question: tuple[Verb, str, str]) -> None:
+        """
+        Update the question components: verb, tense, and pronoun.
+
+        Raises:
+            ValueError: if the question is not a tuple with 3 elements
+                        or if any component is invalid.
+        """
+        if not isinstance(question, tuple) or len(question) != 3:
+            raise ValueError("Question must be tuple with 3 elements: "
+                             "Verb, tense, pronoun")
+
+        question_verb, question_tense, question_pronoun = question
+        if not isinstance(question_verb, Verb):
+            raise ValueError("Question verb must be type Verb")
+
+        if not is_valid_tense(question_tense):
+            raise ValueError("Question tense is not valid")
+
+        if not is_valid_pronoun(question_pronoun):
+            raise ValueError("Question pronoun is not valid")
+
+        self._question_verb = question_verb
+        self._question_tense = question_tense
+        self._question_pronoun = question_pronoun
 
     @property
-    def question_verb(self):
-        return self._question_verb
-
-    @property
-    def question_tense(self):
-        return self._question_tense
-
-    @property
-    def question_pronoun(self):
-        return self._question_pronoun
-
-    @property
-    def answer(self):
+    def answer(self) -> str:
+        """Get the answer."""
         return self._answer
+
+    @answer.setter
+    def answer(self, answer: str) -> None:
+        """
+        Set the answer.
+
+        Raises:
+            ValueError: if the answer is incorrect for the given question.
+        """
+        if self._question_verb.conjugate(self._question_tense,
+                                         self._question_pronoun) != answer:
+            raise ValueError(f"Invalid answer: {answer}")
+        self._answer = answer
--- test_quiz.py
+++ test_quiz.py
@@ -9,3 +9,59 @@ class TestQuizItemCreation(unittest.TestCase):
         verb = Verb("hacer")
         quiz_item = QuizItem((verb, "present", "yo"), "hago")
         self.assertIsInstance(quiz_item, QuizItem)
+
+    def test_quiz_item_creation1(self):
+        """Verifies that newly created QuizItem with valid tense, pronoun, and
+        answer but an invalid verb raises a ValueError."""
+        with self.assertRaises(ValueError) as context:
+            QuizItem(("hacer", "present", "yo"), "hago")
+        self.assertEqual(str(context.exception),
+                         "Question verb must be type Verb")
+
+    def test_quiz_item_creation2(self):
+        """Verifies that newly created QuizItem with valid verb, pronoun, and
+        answer but an invalid tense raises a ValueError."""
+        verb = Verb("hacer")
+        with self.assertRaises(ValueError) as context:
+            QuizItem((verb, "abcd", "yo"), "hago")
+        self.assertEqual(str(context.exception),
+                         "Question tense is not valid")
+
+    def test_quiz_item_creation3(self):
+        """Verifies that newly created QuizItem with valid verb, tense, and
+        answer but an invalid pronoun raises a ValueError."""
+        verb = Verb("hacer")
+        with self.assertRaises(ValueError) as context:
+            QuizItem((verb, "present", "abcd"), "hago")
+        self.assertEqual(str(context.exception),
+                         "Question pronoun is not valid")
+
+    def test_quiz_item_creation4(self):
+        """Verifies that newly created QuizItem with valid verb, tense, and
+        pronoun but an invalid answer raises a ValueError."""
+        verb = Verb("hacer")
+        invalid_answer = "haces"
+        with self.assertRaises(ValueError) as context:
+            QuizItem((verb, "present", "yo"), invalid_answer)
+        self.assertEqual(str(context.exception),
+                         f"Invalid answer: {invalid_answer}")
+
+    def test_quiz_item_creation5(self):
+        """Verifies that newly created QuizItem with an invalid question
+        tuple containing only two elements raises a ValueError."""
+        verb = Verb("hacer")
+        with self.assertRaises(ValueError) as context:
+            QuizItem((verb, "present"), "hago")
+        self.assertEqual(str(context.exception),
+                         "Question must be tuple with 3 elements: "
+                         "Verb, tense, pronoun")
+
+    def test_quiz_item_creation6(self):
+        """Verifies that newly created QuizItem with an invalid question
+        containing only a Verb raises a ValueError."""
+        verb = Verb("hacer")
+        with self.assertRaises(ValueError) as context:
+            QuizItem(verb, "hago")
+        self.assertEqual(str(context.exception),
+                         "Question must be tuple with 3 elements: "
+                         "Verb, tense, pronoun")
