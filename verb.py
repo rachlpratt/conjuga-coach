0775245718b91760aa7c99baaa206d992bb3de53
Rachel Pratt <rachlpratt@gmail.com>
2023-10-09 19:36:02 -0400

    Fix logic in Verb class to validate within setter.

--- verb.py
+++ verb.py
@@ -5,35 +5,52 @@ from irregular_verbs import irregular_verbs
 
 
 class Verb:
-    def __init__(self, infinitive):
-        if not self.is_valid(infinitive):
+    def __init__(self, infinitive: str) -> None:
+        """Initialize a Verb object."""
+        self.infinitive = infinitive
+
+    @property
+    def infinitive(self) -> str:
+        """Get the infinitive form of the verb."""
+        return self._infinitive
+
+    @infinitive.setter
+    def infinitive(self, infinitive: str) -> None:
+        """
+        Set the infinitive form of the verb.
+
+        Raises:
+            ValueError: if the infinitive is not a valid verb.
+        """
+        if infinitive.lower() not in regular_verbs and \
+                infinitive.lower() not in irregular_verbs:
             raise ValueError(f"Invalid verb: {infinitive}")
         self._infinitive = infinitive
 
     @property
-    def infinitive(self):
-        return self._infinitive
-
-    @property
-    def stem(self):
+    def stem(self) -> str:
+        """Get the stem of a verb."""
         return self._infinitive[:-2]
 
     @property
-    def ending(self):
+    def ending(self) -> str:
+        """Get the ending of a verb."""
         endings = ["ar", "er", "ir"]
         if self._infinitive[-2:] in endings:
             return self._infinitive[-2:]
 
     @property
-    def is_regular(self):
+    def is_regular(self) -> bool:
+        """Return True if the verb is regular, otherwise return False."""
         return self._infinitive in regular_verbs
 
-    @classmethod
-    def is_valid(cls, infinitive):
-        return infinitive.lower() in regular_verbs or \
-               infinitive.lower() in irregular_verbs
+    def conjugate(self, tense: str, pronoun: str) -> str:
+        """
+        Conjugate the verb with a given tense and pronoun.
 
-    def conjugate(self, tense, pronoun):
+        Raises:
+            ValueError: if the tense or pronoun is invalid.
+        """
         # Validate tense and pronoun
         if not is_valid_tense(tense):
             raise ValueError("Invalid tense")
@@ -54,5 +71,6 @@ class Verb:
                     callable(conjugation_func := getattr(conjugate, func_name)):
                 return conjugation_func(self, pronoun)
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Get the string representation of the Verb object."""
         return f"Verb: {self.infinitive}"
