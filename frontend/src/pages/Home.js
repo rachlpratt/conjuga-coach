0a76252039236770d1cb9b1374fb36b9dede621c
Rachel Pratt <rachlpratt@gmail.com>
2024-01-31 17:11:23 -0500

    Create basic home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,56 @@
 import React from "react";
+import { Box, Typography } from '@mui/material';
 
 function Home() {
-  return <h2>This is the Home page</h2>;
+  const imagePath = process.env.PUBLIC_URL + '/home_img.jpg';
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        mt: 5,
+        boxShadow: 5,
+        maxWidth: '70%',
+        paddingTop: '30px',
+        mx: 'auto',
+        paddingBottom: '30px',
+      }}
+    >
+      <Box
+        sx={{
+          border: '10px solid white',
+          boxShadow: 5, 
+          borderRadius: '4px',
+          width: '60%',
+          display: 'flex', 
+          justifyContent: 'center',
+          mb: 4
+        }}
+      >
+        <img 
+          src={imagePath} 
+          alt="Home" 
+          style={{ 
+            display: 'block',
+            maxWidth: '100%',
+            height: 'auto',
+            borderRadius: '4px',
+          }} 
+        />
+      </Box>
+      <Box
+        sx={{
+          textAlign: 'center'
+        }}
+      >
+        <Typography variant="h1" component="h2" sx={{ fontSize: '2.25rem' }}>
+          Welcome to ConjugaCoach!
+        </Typography>
+      </Box>
+    </Box>
+  );
 }
 
 export default Home;
